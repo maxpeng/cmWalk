fc82f99e43ffb512a047470b1c2bd96d7897f5a8
Max Peng <max.peng1768@gmail.com>
2017-12-11 23:38:51 +0800

    add command line option to specify the project root directory.

--- fiona.py
+++ fiona.py
@@ -1,13 +1,24 @@
 import sys
 import os
-import walkdir
+import argparse
 from jinja2 import Environment, FileSystemLoader
+import walkdir # https://walkdir.readthedocs.io/en/stable/#
 
-dirTarget = r'''./nucleo_f429zi_freertos_plus_tcp'''
+
+__VERSION__ = '0.0.1'
+
+
+def parseArgs():
+    description = "A python script to generate CMakeLists.txt of a C/C++ project - {}.".format(__VERSION__)
+    parser = argparse.ArgumentParser(description = description)
+    parser.add_argument('input_dir', help = 'The base directory of C/C++ project.')
+    args = parser.parse_args()
+    return args
 
 
 def genTopLevelDirCMakeListsFile(working_path, subdirs, files):
-    """Generate top level CMakeLists.txt.
+    """
+    Generate top level CMakeLists.txt.
 
     :param working_path: current working directory
     :param subdirs: a list of subdirectories of current working directory.
@@ -28,7 +39,8 @@ def genTopLevelDirCMakeListsFile(working_path, subdirs, files):
 
 
 def genSubDirCMakeListsFile(working_path, subdirs, files):
-    """Generate CMakeLists.txt in subdirectories.
+    """
+    Generate CMakeLists.txt in subdirectories.
 
     :param working_path: current working directory
     :param subdirs: a list of subdirectories of current working directory.
@@ -49,12 +61,15 @@ def genSubDirCMakeListsFile(working_path, subdirs, files):
 
 
 def mainFunction():
-    it = walkdir.filtered_walk(dirTarget,
+    args = parseArgs()
+
+    it = walkdir.filtered_walk(args.input_dir,
                               excluded_dirs=['.*', 'build', 'cmake-build-debug'],
                               included_files=['*.s', '*.cpp', '*.c', '*.cxx', '*.h', '*.hpp'])
 
     dir_depth = 0
     for working_path, subdirs, files in it:
+        print("Generating CMakeLists.txt in %s..." % working_path)
         if dir_depth == 0:
             # generate top level CMakeLists.txt.
             genTopLevelDirCMakeListsFile(working_path, subdirs, files)
@@ -62,7 +77,7 @@ def mainFunction():
             # generate CMakeLists.txt in subdirectories.
             genSubDirCMakeListsFile(working_path, subdirs, files)
         dir_depth += 1
-
+    print("\nFinished the generation of CMakeLists.txt files!")
 
 if __name__ == '__main__':
     sys.exit(mainFunction())
