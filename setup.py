d45a23afc001c5d285aa2f82e5d2dfb44804eaca
Max Peng <max.peng1768@gmail.com>
2017-12-20 23:17:04 +0800

    forgot to add setup.py; to build the package: python setup.py bdist_wheel

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+from cmwalk import version
+
+setup(name='cmwalk',
+      version=version.VERSION,
+      description='A python script to walk subdirectories of a C/C++ project of embedded system to generate CMakeLists.txt files for building the executable.',
+      url='http://bitbucket.com/maxpeng260/cmwalk',
+      author='Max Peng',
+      author_email='max.peng1768@gmail.com',
+      license='MIT',
+      packages=['cmwalk'],
+      install_requires=['jinja2', 'walkdir'],
+      package_data={
+          # If any package contains *.txt or *.rst files, include them:
+          'cmwalk': ['*.jinja2'],
+      },
+      entry_points={
+          'console_scripts': [
+              'cmwalk = cmwalk.cmwalk:mainFunction'
+          ]
+      },
+      zip_safe=False)
