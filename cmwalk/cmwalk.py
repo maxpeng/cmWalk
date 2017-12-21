f2ad58c4f731e9bf0924ddada6e93b31553e5293
Max Peng <max.peng1768@gmail.com>
2017-12-21 10:22:30 +0800

    change to implement as a class.

--- cmwalk/cmwalk.py
+++ cmwalk/cmwalk.py
@@ -6,7 +6,7 @@ import argparse
 from jinja2 import Environment, FileSystemLoader
 import json
 import walkdir # https://walkdir.readthedocs.io/en/stable/#
-from cmwalk import version
+from . import version
 
 
 def parseArgs():
@@ -17,71 +17,74 @@ def parseArgs():
     return args
 
 
-
 def getPackagePath():
+    """
+    Return the full path of this package.
+    :return: full path of this package.
+    """
     return os.path.dirname(__file__)
 
 
-def genTopLevelDirCMakeListsFile(working_path, subdirs, files):
-    """
-    Generate top level CMakeLists.txt.
-
-    :param working_path: current working directory
-    :param subdirs: a list of subdirectories of current working directory.
-    :param files: a list of files in current working directory.
-    :return: the full path name of generated CMakeLists.txt.
-    """
-
-    env = Environment(loader=FileSystemLoader(getPackagePath()), trim_blocks=True, lstrip_blocks=True)
-
-    fnameTemplate = 'TopLevel_CMakeLists.txt.jinja2'
-    fnameOut = os.path.join(working_path, 'CMakeLists.txt')
-
-    template = env.get_template(fnameTemplate)
-    fcontent = template.render({'project_name':os.path.basename(os.path.abspath(working_path)), 'subdirs': subdirs, 'files': files})
-    with open(fnameOut, 'w') as f:
-        f.write(fcontent)
-    return fnameOut
-
-
-def genSubDirCMakeListsFile(working_path, subdirs, files):
-    """
-    Generate CMakeLists.txt in subdirectories.
-
-    :param working_path: current working directory
-    :param subdirs: a list of subdirectories of current working directory.
-    :param files: a list of files in current working directory.
-    :return: the full path name of generated CMakeLists.txt.
-    """
-
-    env = Environment(loader=FileSystemLoader(getPackagePath()), trim_blocks=True, lstrip_blocks=True)
-
-    fnameTemplate = 'SubDir_CMakeLists.txt.jinja2'
-    fnameOut = os.path.join(working_path, 'CMakeLists.txt')
-
-    template = env.get_template(fnameTemplate)
-    fcontent = template.render({'subdirs': subdirs, 'files': files})
-    with open(fnameOut, 'w') as f:
-        f.write(fcontent)
-    return fnameOut
-
-
-def readCfgJson(working_path):
-    """Read cmWalk configuration data of a working directory from a json file.
-
-    :param working_path: working path for reading the configuration data.
-    :return: the configuration data represented in a json object, None if the configuration files does not
-             exist.
-    """
-
+class CmWalk(object):
     CFG_JSON_FILENAME = 'cmwalk.json'
-    cfg_json_filename = os.path.join(working_path, CFG_JSON_FILENAME)
-    if os.path.isfile(cfg_json_filename):
-        with open(cfg_json_filename) as json_file:
-            cfg = json.load(json_file)
-            return cfg
-    return None
+    TOP_LEVEL_CMAKELISTS_JINJA2_TEMPLATE = 'TopLevel_CMakeLists.txt.jinja2'
+    SUBDIR_CMAKELISTS_JINJA2_TEMPLATE = 'SubDir_CMakeLists.txt.jinja2'
 
+    def __init__(self):
+        self.envJinja = Environment(loader=FileSystemLoader(getPackagePath()), trim_blocks=True, lstrip_blocks=True)
+
+
+    @classmethod
+    def readCfgJson(cls, working_path):
+        """Read cmWalk configuration data of a working directory from a json file.
+
+        :param working_path: working path for reading the configuration data.
+        :return: the configuration data represented in a json object, None if the configuration files does not
+                 exist.
+        """
+
+        cfg_json_filename = os.path.join(working_path, cls.CFG_JSON_FILENAME)
+        if os.path.isfile(cfg_json_filename):
+            with open(cfg_json_filename) as json_file:
+                cfg = json.load(json_file)
+                return cfg
+        return None
+
+
+    def genTopLevelDirCMakeListsFile(self, working_path, subdirs, files):
+        """
+        Generate top level CMakeLists.txt.
+
+        :param working_path: current working directory
+        :param subdirs: a list of subdirectories of current working directory.
+        :param files: a list of files in current working directory.
+        :return: the full path name of generated CMakeLists.txt.
+        """
+
+        fnameOut = os.path.join(working_path, 'CMakeLists.txt')
+        template = self.envJinja.get_template(self.TOP_LEVEL_CMAKELISTS_JINJA2_TEMPLATE)
+        fcontent = template.render({'project_name':os.path.basename(os.path.abspath(working_path)), 'subdirs': subdirs, 'files': files})
+        with open(fnameOut, 'w') as f:
+            f.write(fcontent)
+        return fnameOut
+
+
+    def genSubDirCMakeListsFile(self, working_path, subdirs, files):
+        """
+        Generate CMakeLists.txt in subdirectories.
+
+        :param working_path: current working directory
+        :param subdirs: a list of subdirectories of current working directory.
+        :param files: a list of files in current working directory.
+        :return: the full path name of generated CMakeLists.txt.
+        """
+
+        fnameOut = os.path.join(working_path, 'CMakeLists.txt')
+        template = self.envJinja.get_template(self.SUBDIR_CMAKELISTS_JINJA2_TEMPLATE)
+        fcontent = template.render({'subdirs': subdirs, 'files': files})
+        with open(fnameOut, 'w') as f:
+            f.write(fcontent)
+        return fnameOut
 
 
 def mainFunction():
@@ -90,12 +93,14 @@ def mainFunction():
     it = walkdir.filtered_walk(args.input_dir,
                               included_files=['*.s', '*.cpp', '*.c', '*.cxx', '*.h', '*.hpp'])
 
+    cmwalk = CmWalk()
+
     dir_depth = 0
     for working_path, subdirs, files in it:
         print("Generating CMakeLists.txt in %s..." % working_path)
 
         # read configuration data of the working path form the json file, 'cfg' will be None if it does not exist.
-        cfg = readCfgJson(working_path)
+        cfg = cmwalk.readCfgJson(working_path)
         if cfg:
             if 'sourceDirs' in cfg.keys():
                 includedSourceDirs = []
@@ -119,10 +124,10 @@ def mainFunction():
 
         if dir_depth == 0:
             # generate top level CMakeLists.txt.
-            genTopLevelDirCMakeListsFile(working_path, subdirs, files)
+            cmwalk.genTopLevelDirCMakeListsFile(working_path, subdirs, files)
         else:
             # generate CMakeLists.txt in subdirectories.
-            genSubDirCMakeListsFile(working_path, subdirs, files)
+            cmwalk.genSubDirCMakeListsFile(working_path, subdirs, files)
         dir_depth += 1
     print("\nFinished the generation of CMakeLists.txt files!")
 
